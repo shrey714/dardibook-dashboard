430fbf982b8cd12902cada9a29836a14faf970b5
Shrey Patel <76216765+shrey714@users.noreply.github.com>
2024-08-19 23:48:56 +0530

    get all plans problem

--- src/app/services/razorpay/getplans.js
+++ src/app/services/razorpay/getplans.js
@@ -1,9 +1,14 @@
 import { NextResponse } from "next/server";
 
 const getPlans = async () => {
-  const plans = await fetch(
-    "https://backend.dardibook.in/allPlans"
-  );
+  const plans = await fetch("https://backend.dardibook.in/allPlans", {
+    method: "GET",
+    headers: {
+      // You can include any additional headers if needed
+    },
+  });
+
+  
   const parsedPlans = await plans.json();
   return NextResponse.json(parsedPlans);
 };
