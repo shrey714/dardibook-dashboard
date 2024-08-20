8100433583da10c38415089bc5e33145cca10ddf
Shrey Patel <76216765+shrey714@users.noreply.github.com>
2024-08-20 20:31:10 +0530

    Update getplans.js

--- src/app/services/razorpay/getplans.js
+++ src/app/services/razorpay/getplans.js
@@ -1,8 +1,15 @@
 import { NextResponse } from "next/server";
 
 const getPlans = async () => {
-  const plans = await fetch("https://backend.dardibook.in/allPlans");
-
+  // const plans = await fetch("https://backend.dardibook.in/allPlans");
+const plans = await fetch('https://backend.dardibook.in/allPlans', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            // You can also add any authorization headers if needed
+          },
+          credentials: 'include', // Allows sending cookies along with the request if needed
+        });
   
   const parsedPlans = await plans.json();
   return NextResponse.json(parsedPlans);
