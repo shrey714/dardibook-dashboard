80b2f350aaaa75e3d59c1642f8b13efc35ac9fe2
shrey dardibook <shrey@dardibook.in>
2024-09-19 21:04:42 +0530

    settings small fixes

--- src/components/Settings/FooterLine.tsx
+++ src/components/Settings/FooterLine.tsx
@@ -1,14 +1,7 @@
-import Link from "next/link";
 import React from "react";
 
 const FooterLine = () => {
-  const allPaths = [
-    "cancellation-policy",
-    "contact-us",
-    "pricing",
-    "privacy-policy",
-    "terms-conditions",
-  ];
+
   return (
     <div className="mt-3 md:mt-6 mx-auto max-w-4xl px-3 py-2 md:px-8 text-center">
       <p className="text-xs sm:text-sm font-medium leading-3 text-gray-500 mx-3">
