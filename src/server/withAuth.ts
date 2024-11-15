9eb917bbbf01996dbc68fa4ee7042fc415000931
JeetOza31 <jeet@dardibook.in>
2024-11-16 02:39:47 +0530

    admin initialisation changes

--- src/server/withAuth.ts
+++ src/server/withAuth.ts
@@ -9,6 +9,16 @@ import admin from "firebase-admin";
 //   });
 // }
 
+if (admin.apps.length === 0) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+}
+
 export const withAuth = (handler: (arg0: any, arg1: any) => any) => {
   return async (request:any, context: any) => {
     const authHeader = request.headers.get("authorization");
