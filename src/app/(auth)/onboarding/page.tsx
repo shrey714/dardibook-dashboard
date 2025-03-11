cff1f8203dd856fd0d486c2e272463ac1b610d5c
shrey dardibook <shrey@dardibook.in>
2025-03-11 14:01:16 +0530

    build fix

--- src/app/(auth)/onboarding/page.tsx
+++ src/app/(auth)/onboarding/page.tsx
@@ -1,15 +1,15 @@
 "use server";
 
-// import { useRouter, useSearchParams } from "next/navigation";
-// import { AnimatePresence } from "framer-motion";
-// import Intro from "@/components/Welcome/intro";
-// import Next from "@/components/Welcome/next";
-// import Select from "@/components/Welcome/select";
-// import Dataroom from "@/components/Welcome/dataroom";
+import { AnimatePresence } from "framer-motion";
+import Intro from "@/components/Welcome/intro";
+import Next from "@/components/Welcome/next";
+import Select from "@/components/Welcome/select";
+import Dataroom from "@/components/Welcome/dataroom";
 import {
-  // BackButton,
+  BackButton,
   OnboardCompletionButton,
 } from "@/components/Welcome/BackButton";
+import { Suspense } from "react";
 
 export default async function Page({
   searchParams,
@@ -21,12 +21,14 @@ export default async function Page({
   return (
     <div className="mx-auto flex h-screen max-w-3xl flex-col items-center justify-center overflow-x-hidden">
       <OnboardCompletionButton />
-      {/* <AnimatePresence mode="wait">
-        {type ? <BackButton /> : <Intro key="intro" />}
-        {type === "next" && <Next key="next" />}
-        {type === "select" && <Select key="select" />}
-        {type === "dataroom" && <Dataroom key="dataroom" />}
-      </AnimatePresence> */}
+      <AnimatePresence mode="wait">
+        <Suspense>
+          {type ? <BackButton /> : <Intro key="intro" />}
+          {type === "next" && <Next key="next" />}
+          {type === "select" && <Select key="select" />}
+          {type === "dataroom" && <Dataroom key="dataroom" />}
+        </Suspense>
+      </AnimatePresence>
     </div>
   );
 }
