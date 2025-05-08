0b53fb01f8d10576d7ef6152547c3be3423a4bf1
shrey dardibook <shrey@dardibook.in>
2025-05-09 00:27:41 +0530

    layout changes(will start to work on history pages)

--- src/app/dashboard/history/loading.tsx
+++ src/app/dashboard/history/loading.tsx
@@ -1,9 +1,13 @@
-import Loader from "@/components/common/Loader";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="w-full h-full overflow-hidden flex items-center justify-center">
-      <Loader size="medium" />
+    <div className="w-full grid auto-rows-[22rem] grid-cols-3 gap-4 lg:grid-rows-4 p-4 min-h-full">
+      <Skeleton className="lg:row-span-2 lg:col-span-1" />
+      <Skeleton className="lg:row-span-4 lg:col-span-1" />
+      <Skeleton className="lg:row-span-2 lg:col-span-1" />
+      <Skeleton className="lg:row-span-2 lg:col-span-1" />
+      <Skeleton className="lg:row-span-2 lg:col-span-1" />
     </div>
   );
 }
