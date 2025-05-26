280cc9ef5959b8b33c9895ccac9dd43743d4ec5b
shrey dardibook <shrey@dardibook.in>
2025-05-26 20:36:58 +0530

    api access fix

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -29,7 +29,7 @@ export default async function Home({ searchParams }: PageProps) {
       `${baseUrl}/api/dashboard-data?weekDate=${weekTimestamp}`,
       {
         method: "GET",
-        headers: await headers(),
+        headers: headersList,
         cache: "no-store",
       }
     );
