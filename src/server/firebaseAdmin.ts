e6dc3e22210458c6d1555a6a08b3b1d4f013488b
shrey dardibook <shrey@dardibook.in>
2025-06-21 05:47:08 +0530

    fixed firebase admin

--- src/server/firebaseAdmin.ts
+++ src/server/firebaseAdmin.ts
@@ -1,11 +1,18 @@
 import admin from "firebase-admin";
 
 if (admin.apps.length === 0) {
+
+
+  console.log("datass===", process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    process.env.FIREBASE_CLIENT_EMAIL,
+    process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"))
+
+
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.split(String.raw`\n`).join('\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
   });
 }
